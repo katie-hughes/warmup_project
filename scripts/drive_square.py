823dc4e8609c37a78f427b63bb0ed3395d91a8a7
Katherine Rees Hughes <khughes1@linux1.cs.uchicago.edu>
2021-01-18 03:16:51 -0600

    logic for driving in a square is implemented but I don't see anything on gazebo yet

--- scripts/drive_square.py
+++ scripts/drive_square.py
@@ -1,4 +1,46 @@
 #!/usr/bin/env python3
-""" Description """
 import rospy
-from geometry_msgs.msg import PointStamped
+
+from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Vector3
+
+import time
+import math
+
+class DriveInSquare(object):
+	""" Description """
+	def __init__(self):
+		rospy.init_node("drive_square")
+		#rospy.Subscriber("/time", float, time)
+		self.twist_pub = rospy.Publisher("/cmd_vel", Twist, queue_size = 10)
+		r = rospy.Rate(2)
+		lin = Vector3()
+		ang = Vector3()
+		self.twist = Twist(linear=lin, angular = ang)
+		print(self.twist)
+	def run(self):
+		print("running")
+		while not rospy.is_shutdown():
+			self.twist.linear.x = 0.1
+			self.twist_pub.publish(self.twist)
+			print(self.twist)
+			print("sleeping for 5")
+			time.sleep(5.0)
+
+			self.twist.linear.x = 0.0
+			self.twist_pub.publish(self.twist)
+			self.twist.angular.z = 0.5
+			self.twist_pub.publish(self.twist)
+			print(self.twist)
+			print("sleeping for pi")
+			time.sleep(math.pi)
+
+			self.twist.angular.z = 0.0
+			self.twist_pub.publish(self.twist)
+			print(self.twist)
+			print('successfully turned')
+		#rospy.spin()
+
+if __name__ == "__main__":
+	node = DriveInSquare()
+	node.run()

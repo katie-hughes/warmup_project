24a1772b9635fe95823f94cd7a5bcb07d16f1dae
Katherine Rees Hughes <khughes1@linux1.cs.uchicago.edu>
2021-01-23 21:10:46 -0600

    robot almost made it a full lap before getting stuck in the corner

--- scripts/wall_follower.py
+++ scripts/wall_follower.py
@@ -37,7 +37,7 @@ class FollowWall(object):
             print("The robot is the correct distance from the wall")
             if 270-delta <= minimum_angle <= 270+delta:
                 print("The robot is facing the right way")
-                error4 = 270 - minimum_angle
+                error4 = minimum_angle - 270
                 kp4 = 0.01
                 self.twist.linear.x = speed
                 self.twist.angular.z = error4*kp4
@@ -45,7 +45,7 @@ class FollowWall(object):
             else:
                 print("The robot needs to turn")
                 error1 = 0
-                if 90<=minimum_angle<=270:      ##turn right
+                if 90<=minimum_angle<=270:
                     print("Turn right")
                     error1 = minimum_angle - 270
                 else:
@@ -59,34 +59,32 @@ class FollowWall(object):
                 self.twist.linear.x = 0
                 self.twist.angular.z = kp1*error1
                 self.twist_pub.publish(self.twist)
-        elif minimum_distance > dist+buffer:
+        elif minimum_distance > dist:
             print("The robot is too far away")
-            if minimum_angle <= delta or minimum_angle >= (360-delta):
-                print("The robot is facing the appropriate wall")
-                error2 = minimum_distance - dist
-                kp2 = 1.0
-                self.twist.linear.x = kp2*error2
-                self.twist.angular.z = 0
-                self.twist_pub.publish(self.twist)
+            kp_speed = 0.3
+            kp_angle = 0.02
+            error_speed = minimum_distance   # - dist
+            error_angle = 0
+            if minimum_angle <= 180:
+                print('The closest wall is on the left')
+                error_angle = minimum_angle
             else:
-                print("The robot needs to turn to face the appropriate wall")
-                error3 = 0
-                if minimum_angle <= 180:    ## closest wall on the left
-                    print("closest wall is on the left")
-                    error3 = minimum_angle
-                else:                       ## closest wall on the right
-                    error3 = minimum_angle - 360
-                    print("closest wall is on the right")
-                kp3 = 0.01
-                print("error3 is", error3)
-                print("new angular velocity is", kp3*error3)
+                print('The closest wall is on the right')
+                error_angle =minimum_angle - 360
+            print("Error angle is", error_angle)
+            self.twist.linear.x = error_speed*kp_speed
+            if delta <= minimum_angle <= (360-delta):
                 self.twist.linear.x = 0
-                self.twist.angular.z = kp3*error3
-                self.twist_pub.publish(self.twist)
-                ## i think this portion generally works.
+            self.twist.angular.z = error_angle*kp_angle
+            self.twist_pub.publish(self.twist)
+
         else:
             print("The robot is too close")
             ##robot needs to turn away from the wall
+            self.twist.angular.z = 0.1
+            self.twist.linear.x = 0.1
+            self.twist_pub.publish(self.twist)
+            """
             if minimum_angle < 300:
                 print("Robot needs to trun away from the wall ")
                 error5 = 300 - minimum_angle
@@ -98,6 +96,7 @@ class FollowWall(object):
                 self.twist.linear.x = speed
                 self.twist.angular.z = 0
                 self.twist_pub.publish(self.twist)
+            """
 
 
     def run(self):

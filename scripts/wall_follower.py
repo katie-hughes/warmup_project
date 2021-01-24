171fd8a25cfe986fcfbb7693dc97015c913af0e2
Katherine Rees Hughes <khughes1@linux1.cs.uchicago.edu>
2021-01-23 19:01:33 -0600

    still struggling with the corner cases in wall follower

--- scripts/wall_follower.py
+++ scripts/wall_follower.py
@@ -10,9 +10,10 @@ import math
 
 
 angles = np.arange(360)
-dist = 0.3
-buffer = 0.15
-delta = 10
+dist = 0.5
+buffer = 0.2
+delta = 5  ##allowed angle offset
+speed = 0.2
 
 class FollowWall(object):
     """ description """
@@ -27,28 +28,19 @@ class FollowWall(object):
     def process_scan(self, data):
         ## len of data.ranges is 360
         print()
-        minimum = np.argmin(np.array(data.ranges))
-        minimum_distance = data.ranges[minimum]
-        minimum_angle = angles[minimum]
-        #print("the minimum index is", minimum)
+        minimum_angle = np.argmin(np.array(data.ranges))
+        minimum_distance = data.ranges[minimum_angle]
+        front_distance = data.ranges[0]
         print("the minimum distance is", minimum_distance)
         print("the minimum angle is", minimum_angle)
-        """
-        Now what do I want the robot to do?
-        I know what direction it needs to turn to face the closest wall
-        The first step is to drive that direction
-        which means turn towards the appropriate angle and also drive towards
-        Once the robot gets a certain distance away from the wall it should
-        just drive straight (say 0.4 m or something).
-        If driving along straight wall
-        the goal angle should be 90 +/- delta.
-        """
         if dist-buffer <= minimum_distance <= dist+buffer:
             print("The robot is the correct distance from the wall")
             if 270-delta <= minimum_angle <= 270+delta:
                 print("The robot is facing the right way")
-                self.twist.linear.x = 0.1
-                self.twist.angular.z = 0
+                error4 = 270 - minimum_angle
+                kp4 = 0.01
+                self.twist.linear.x = speed
+                self.twist.angular.z = error4*kp4
                 self.twist_pub.publish(self.twist)
             else:
                 print("The robot needs to turn")
@@ -63,14 +55,13 @@ class FollowWall(object):
                     else:
                         error1 = 90 + minimum_angle
                 kp1 = 0.01
-                print("ang velocity: ", error1)
+                print("ang velocity: ", error1*kp1)
                 self.twist.linear.x = 0
                 self.twist.angular.z = kp1*error1
                 self.twist_pub.publish(self.twist)
-        else:
-            print("The robot is too far away from the wall")
-            ##robot needs to turn to face the wall
-            if minimum_angle <= 10 or minimum_angle >= 350:
+        elif minimum_distance > dist+buffer:
+            print("The robot is too far away")
+            if minimum_angle <= delta or minimum_angle >= (360-delta):
                 print("The robot is facing the appropriate wall")
                 error2 = minimum_distance - dist
                 kp2 = 1.0
@@ -93,6 +84,21 @@ class FollowWall(object):
                 self.twist.angular.z = kp3*error3
                 self.twist_pub.publish(self.twist)
                 ## i think this portion generally works.
+        else:
+            print("The robot is too close")
+            ##robot needs to turn away from the wall
+            if minimum_angle < 300:
+                print("Robot needs to trun away from the wall ")
+                error5 = 300 - minimum_angle
+                kp5 = 0.01
+                self.twist.linear.x = 0
+                self.twist.angular.z = error5*kp5
+                self.twist_pub.publish(self.twist)
+            else:
+                self.twist.linear.x = speed
+                self.twist.angular.z = 0
+                self.twist_pub.publish(self.twist)
+
 
     def run(self):
         rospy.spin()

1c5c2aaa7f3dccdd923fbdd09ebb297cbcfa3f9d
Katherine Rees Hughes <khughes1@linux1.cs.uchicago.edu>
2021-01-24 23:53:27 -0600

    fixed bug where fobot got stuck in corners

--- scripts/wall_follower.py
+++ scripts/wall_follower.py
@@ -12,7 +12,7 @@ import math
 angles = np.arange(360)
 dist = 0.5
 buffer = 0.2
-delta = 5  ##allowed angle offset
+delta = 10  ##allowed angle offset
 speed = 0.2
 
 class FollowWall(object):
@@ -25,70 +25,82 @@ class FollowWall(object):
         lin = Vector3()
         ang = Vector3()
         self.twist = Twist(linear=lin, angular = ang)
+    def change_speed(self, linear_velocity, angular_velocity):
+        self.twist.linear.x = linear_velocity
+        self.twist.angular.z = angular_velocity
+        self.twist_pub.publish(self.twist)
     def process_scan(self, data):
         print()
         minimum_angle = np.argmin(np.array(data.ranges))
         minimum_distance = data.ranges[minimum_angle]
         front_distance = data.ranges[0]
+        front_right_distance = data.ranges[345]
         print("the minimum distance is", minimum_distance)
         print("the minimum angle is", minimum_angle)
-        if dist-buffer <= minimum_distance <= dist+buffer:
-            print("The robot is the correct distance from the wall")
-            if 270-delta <= minimum_angle <= 270+delta:
-                print("The robot is facing the right way")
-                error4 = minimum_angle - 270
-                kp4 = 0.005
-                self.twist.linear.x = speed
-                self.twist.angular.z = error4*kp4
-                self.twist_pub.publish(self.twist)
-            else:
-                print("The robot needs to turn")
-                error1 = 0
-                if 90<=minimum_angle<=270:
-                    print("Turn Left (+))")
-                    error1 = minimum_angle - 270
-                else:
-                    print("Turn Right (-)")
-                    if minimum_angle > 0:
-                        error1 = minimum_angle - 270
-                    else:
-                        error1 = 90 + minimum_angle
-                kp1 = 0.01
-                print("ang velocity: ", error1*kp1)
-                self.twist.linear.x = 0
-                self.twist.angular.z = kp1*error1
-                self.twist_pub.publish(self.twist)
-        elif minimum_distance > dist:
-            print("The robot is too far away")
-            kp_speed = 0.3
-            kp_angle = 0.02
-            error_speed = minimum_distance   # - dist
-            error_angle = 0
-            if minimum_angle <= 180:
-                print('The closest wall is on the left')
-                error_angle = minimum_angle
-            else:
-                print('The closest wall is on the right')
-                error_angle =minimum_angle - 360
-            print("Error angle is", error_angle)
-            self.twist.linear.x = error_speed*kp_speed
-            if delta <= minimum_angle <= (360-delta):
-                self.twist.linear.x = 0
-            self.twist.angular.z = error_angle*kp_angle
-            self.twist_pub.publish(self.twist)
-
+        print("the front distance is", front_distance)
+        if front_distance < (dist-buffer) or front_right_distance < (dist-buffer):
+            print("TOO CLOSE")
+            self.change_speed(0, 0.8)
         else:
-            print("The robot is too close")
-            ##robot needs to turn away from the wall
-            if front_distance < dist:
-                print("Robot is about to hit a wall")
-                self.twist.linear.x = 0
-                self.twist.angular.z = 0.5
-                self.twist_pub.publish(self.twist)
+            if dist-buffer <= minimum_distance <= dist+buffer:
+                print("The robot is the correct distance from the wall")
+                if 270-delta <= minimum_angle <= 270+delta:
+                    print("The robot is facing the right way")
+                    error1 = minimum_angle - 270
+                    kp1 = 0.005
+                    self.change_speed(speed, error1*kp1)
+                else:
+                    ## The robot needs to turn
+                    print("The robot needs to turn")
+                    error2 = 0
+                    if 90<=minimum_angle<=270:
+                        ## The wall is behind the robot. Turn Right (- ang mom)
+                        print("")
+                        error2 = minimum_angle - 270
+                    else:
+                        ## The wall is in front of the robot. Turn Left (+ ang mom)
+                        if minimum_angle > 270:
+                            print("angle", minimum_angle)
+                            error2 = minimum_angle - 270
+                            print("error2:", error2)
+                        else:
+                            error2 = 90 + minimum_angle
+                    kp2 = 0.01
+                    print("ang velocity: ", error2*kp2)
+                    self.change_speed(0, kp2*error2)
+            elif minimum_distance > dist+buffer :
+                ## The robot is too far away from the wall
+                print("The robot is too far away")
+                kp_speed = 0.3
+                kp_angle = 0.02
+                error_speed = minimum_distance   # - dist
+                error_angle = 0
+                if minimum_angle <= 180:
+                    ## The closest wall is on the left, robot needs to turn left
+                    print('The closest wall is on the left')
+                    error_angle = minimum_angle
+                else:
+                    ## The closest wall is on the right, robot needs to turn right
+                    print('The closest wall is on the right')
+                    error_angle =minimum_angle - 360
+                print("Error angle is", error_angle)
+                if delta <= minimum_angle <= (360-delta):
+                    ## robot pauses until it is facing the wall
+                    self.change_speed(0, error_angle*kp_angle)
+                else:
+                    ## robot drives straight towards the closest wall
+                    self.change_speed(error_speed*kp_speed, error_angle*kp_angle)
+
             else:
-                self.twist.angular.z = 0.1
-                self.twist.linear.x = speed
-                self.twist_pub.publish(self.twist)
+                print("The robot is too close")
+                if front_distance < (dist+buffer) or front_right_distance < (dist+buffer):
+                    ## there is something immediately in front of the robot
+                    print("Something in front of robot")
+                    self.change_speed(0, 0.5)
+                else:
+                    print("Something to side")
+                    # the robot's right side is very close to the wall.
+                    self.change_speed(speed, 0.1)
 
 
     def run(self):

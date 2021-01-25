78d0fbdd82cb242a5859ef8309a03b72973cf3a2
Katherine Rees Hughes <khughes1@linux1.cs.uchicago.edu>
2021-01-25 02:12:54 -0600

    more documentaition for person follower

--- scripts/person_follower.py
+++ scripts/person_follower.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-""" Description """
 import rospy
 from geometry_msgs.msg import Twist
 from geometry_msgs.msg import Vector3
@@ -9,55 +8,57 @@ import time
 import math
 
 
-dist = 0.4
-
-delta = 15
+dist = 0.5          ## The desired distance from the object
+delta = 15          ## Allowed angle offset when determining angle to object
 
 class FollowPerson(object):
-    """ description """
+    """ Robot locates the closest object (person) to itself
+        and travels towards it, stopping at 'dist' m. away from it.
+    """
     def __init__(self):
         rospy.init_node("follow_wall")
+        ## subscribe to data from robot's sensors
         rospy.Subscriber("/scan", LaserScan, self.process_scan)
+        ## set up publishing node to cmd_velocity to move/rotate robot
         self.twist_pub = rospy.Publisher("/cmd_vel", Twist, queue_size = 10)
         r = rospy.Rate(2)
         lin = Vector3()
         ang = Vector3()
         self.twist = Twist(linear=lin, angular = ang)
+    def change_velocity(self, linear_velocity, angular_velocity):
+        ## Change the linear and angular velocity of the robot
+        self.twist.linear.x = linear_velocity
+        self.twist.angular.z = angular_velocity
+        self.twist_pub.publish(self.twist)
     def process_scan(self, data):
-        ## len of data.ranges is 360
-        print()
+        ## Reads the sensor data to determine how to move
+        ## The angle and distance to the closest object (person):
         minimum_angle = np.argmin(np.array(data.ranges))
         minimum_distance = data.ranges[minimum_angle]
-        front_distance = data.ranges[0]
-        print("the minimum distance is", minimum_distance)
-        print("the minimum angle is", minimum_angle)
         if minimum_distance == float('inf'):
-            print("person not found")
-            self.twist.linear.x = 0
-            self.twist.angular.z = 0
-            self.twist_pub.publish(self.twist)
+            ## The robot can't locate a nearest object, so it halts
+            self.change_velocity(0, 0)
         elif minimum_distance > dist:
-            kp_speed = 0.5
+            ## The robot is too far away from the object, moves closer.
+            kp_speed = 0.3
             kp_angle = 0.02
-            error_speed = minimum_distance - dist
+            error_speed = minimum_distance
             error_angle = 0
             if minimum_angle <= 180:
-                print('The person is on the left')
+                ## The object is on the left, turn left (error, ang mom > 0)
                 error_angle = minimum_angle
             else:
-                print('The person is on the right')
-                error_angle =minimum_angle - 360
-            print("Error angle is", error_angle)
-            self.twist.linear.x = error_speed*kp_speed
+                ## The object is on the right, turn right (error, ang mom > 0)
+                error_angle = minimum_angle - 360
             if delta <= minimum_angle <= (360-delta):
-                self.twist.linear.x = 0
-            self.twist.angular.z = error_angle*kp_angle
-            self.twist_pub.publish(self.twist)
+                ## the robot needs to rotate to face the object
+                self.change_velocity(0, error_angle*kp_angle)
+            else:
+                ## the robot is roughly facing the object, moves forward
+                self.change_velocity(error_speed*kp_speed, error_angle*kp_angle)
         else:
-            self.twist.linear.x = 0
-            self.twist.angular.z = 0
-            self.twist_pub.publish(self.twist)
-
+            ## The robot is within 'dist' of the object, so it can stop
+            self.change_velocity(0, 0)
     def run(self):
         rospy.spin()
 
